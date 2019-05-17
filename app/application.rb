eeaa432532293306b284355c2dc9e4ed35258a20
Ming Duan <11203duan@gmail.com>
2019-05-17 20:27:28 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -5,7 +5,7 @@ def call
 
   resp = Rack::Response.new
 
-  if time < 12:00
+  if time < Time.parse('12:00:00 +0100')
     resp.write "Good Morning"
   elsif 12:00 < time < 24:00
     resp.write "Good Afternoon"

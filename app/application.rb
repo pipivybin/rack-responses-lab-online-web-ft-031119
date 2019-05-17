41aeb4e3d6f43cb2a5f7bfc6b951e1ed0fda29bb
Ming Duan <11203duan@gmail.com>
2019-05-17 20:16:55 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -1,9 +1,13 @@
 class Application
 
 def call
-  get '/' do
-    
-  end
+  time = Time.now
+
+  if time < 12 pm
+    resp.write "Good Morning"
+  else 
+    resp.write "Good Afternoon" 
+  
 end
 
 end
